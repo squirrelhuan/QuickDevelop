0f277f8956a9e3d2c872b1d7ec9037d9fdb81f8e
squirrelhuan <978252909@qq.com>
2018-11-07 16:00:50 +0800

    cgq

--- quickdeveloplibrary/src/main/java/cn/demomaster/huan/quickdeveloplibrary/util/StringVerifyUtil.java
+++ quickdeveloplibrary/src/main/java/cn/demomaster/huan/quickdeveloplibrary/util/StringVerifyUtil.java
@@ -1,12 +1,62 @@
 package cn.demomaster.huan.quickdeveloplibrary.util;
 
+import android.app.Activity;
+import android.text.TextUtils;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ *
+ */
 public class StringVerifyUtil {
 
-    boolean verifyTelePhone(){
-        return false;
+    /**
+     * 电话号码格式校验
+     * @param telephone
+     * @return
+     */
+    public static boolean validateTelePhone(String telephone){
+        if (TextUtils.isEmpty(telephone) || !isTelePhoneValid(telephone)) {
+            return false;
+        }
+        return true;
     }
-    boolean verifyMobilePhone(){
-        return false;
+
+    /**
+     * 手机号码格式校验
+     * @param telephone
+     * @return
+     */
+    public static boolean validateMobilePhone(String telephone){
+        if (TextUtils.isEmpty(telephone) || !isTelePhoneValid(telephone)) {
+            return false;
+        }
+        return true;
     }
+
+    /**
+     * 密码格式校验
+     * @param password
+     * @return
+     */
+    public static boolean validatePassword(String password){
+        if (TextUtils.isEmpty(password) || !isPasswordValid(password)) {
+            return false;
+        }
+        return true;
+    }
+    //手机格式验证
+    public static boolean isTelePhoneValid(String telephone) {
+        Pattern p = Pattern.compile("^((14[0-9])|(13[0-9])|(15[0-9])|(16[0-9])|(18[0-9])|(17[0-9]))\\d{8}$");
+        Matcher m = p.matcher(telephone);
+        return m.matches();
+    }
+
+    //密码长度判断大于4
+    public static boolean isPasswordValid(String password) {
+        return password.length() >= 4;
+    }
+
 
 }
