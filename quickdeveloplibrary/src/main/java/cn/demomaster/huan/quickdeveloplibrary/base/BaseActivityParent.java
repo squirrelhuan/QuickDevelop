bcf5bb8ec678ca0a91e6e0b56bfb835c79eb884d
squirrelhuan <978252909@qq.com>
2018-11-07 15:21:30 +0800

    cgq

--- quickdeveloplibrary/src/main/java/cn/demomaster/huan/quickdeveloplibrary/base/BaseActivityParent.java
+++ quickdeveloplibrary/src/main/java/cn/demomaster/huan/quickdeveloplibrary/base/BaseActivityParent.java
@@ -2,9 +2,13 @@ package cn.demomaster.huan.quickdeveloplibrary.base;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.ActivityOptionsCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 import cn.demomaster.huan.quickdeveloplibrary.ApplicationParent;
 import cn.demomaster.huan.quickdeveloplibrary.util.StatusBarUtil;
@@ -19,14 +23,29 @@ public class BaseActivityParent extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         mContext = this;
         mBundle = getIntent().getExtras();
-        ((ApplicationParent)getApplicationContext()).addActivity(this);
+        ((ApplicationParent) getApplicationContext()).addActivity(this);
+    }
+
+    public View getContentView() {
+        return this.findViewById(android.R.id.content);
     }
 
     /**
      * 设置导航栏透明
      */
-    public void transparentBar(){
+    public void transparentBar() {
         StatusBarUtil.transparencyBar(this);
     }
+
+    public void startActivity(Class<?> clazz) {
+        startActivity(clazz, null);
+    }
+
+    public void startActivity(Class<?> clazz, Bundle bundle) {
+        Intent intent = new Intent(this, clazz);
+        if (bundle != null) intent.putExtras(bundle);
+        startActivity(intent);
+    }
+
 
 }
