1b07c49c78b90574e2fc243421381ea8f5321b5e
squirrelhuan <978252909@qq.com>
2018-11-07 12:39:51 +0800

    Initial commit

---
+++ app/src/main/java/cn/demomaster/huan/quickdevelop/MainActivity.java
@@ -0,0 +1,13 @@
+package cn.demomaster.huan.quickdevelop;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
