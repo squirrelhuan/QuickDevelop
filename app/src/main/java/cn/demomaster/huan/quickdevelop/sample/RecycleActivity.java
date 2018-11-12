4f73c8fdbb7c0126ac9e4afc19cf26aeec96d430
squirrelhuan <978252909@qq.com>
2018-11-12 10:16:01 +0800

    cgq

--- app/src/main/java/cn/demomaster/huan/quickdevelop/sample/RecycleActivity.java
+++ app/src/main/java/cn/demomaster/huan/quickdevelop/sample/RecycleActivity.java
@@ -1,5 +1,6 @@
 package cn.demomaster.huan.quickdevelop.sample;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
@@ -8,6 +9,7 @@ import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.helper.ItemTouchHelper;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import cn.demomaster.huan.quickdevelop.R;
@@ -18,28 +20,14 @@ public class RecycleActivity extends AppCompatActivity {
     private RecyclerView recy_drag;
     private LinearLayoutManager linearLayoutManager;
     private List<String> lists;
+    private RecycleViewAdapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recycle);
 
         recy_drag=findViewById(R.id.recy_drag);
-        ItemTouchHelper mItemTouchHelper = new ItemTouchHelper(new ItemTouchHelper.Callback() {
-            @Override
-            public int getMovementFlags(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder) {
-                return 0;
-            }
-
-            @Override
-            public boolean onMove(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, RecyclerView.ViewHolder target) {
-                return false;
-            }
-
-            @Override
-            public void onSwiped(RecyclerView.ViewHolder viewHolder, int direction) {
-
-            }
-        });
+        ItemTouchHelper mItemTouchHelper = new ItemTouchHelper(new CallBack());
         mItemTouchHelper.attachToRecyclerView(recy_drag);
 
         //模拟一些数据加载
@@ -49,13 +37,80 @@ public class RecycleActivity extends AppCompatActivity {
         }
         //这里使用线性布局像listview那样展示列表,第二个参数可以改为 HORIZONTAL实现水平展示
         linearLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
-//使用网格布局展示
+        //使用网格布局展示
         recy_drag.setLayoutManager(new GridLayoutManager(this, 5));
         //recy_drag.setLayoutManager(linearLayoutManager);
-        final RecycleViewAdapter adapter = new RecycleViewAdapter(this, lists);
+        adapter = new RecycleViewAdapter(this, lists);
         //设置分割线使用的divider
         recy_drag.addItemDecoration(new android.support.v7.widget.DividerItemDecoration(this, android.support.v7.widget.DividerItemDecoration.VERTICAL));
         recy_drag.setAdapter(adapter);
+    }
+
+    public class CallBack extends ItemTouchHelper.Callback{
+
+        @Override
+        public int getMovementFlags(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder) {
+            if (recyclerView.getLayoutManager() instanceof GridLayoutManager) {
+                final int dragFlags = ItemTouchHelper.UP | ItemTouchHelper.DOWN |
+                        ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT;
+                final int swipeFlags = 0;
+                return makeMovementFlags(dragFlags, swipeFlags);
+            } else {
+                final int dragFlags = ItemTouchHelper.UP | ItemTouchHelper.DOWN;
+                final int swipeFlags = 0;
+                return makeMovementFlags(dragFlags, swipeFlags);
+            }
+        }
+
+        @Override
+        public boolean onMove(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, RecyclerView.ViewHolder target) {
+            //得到当拖拽的viewHolder的Position
+            int fromPosition = viewHolder.getAdapterPosition();
+            //拿到当前拖拽到的item的viewHolder
+            int toPosition = target.getAdapterPosition();
+            if (fromPosition < toPosition) {
+                for (int i = fromPosition; i < toPosition; i++) {
+                    Collections.swap(lists, i, i + 1);
+                }
+            } else {
+                for (int i = fromPosition; i > toPosition; i--) {
+                    Collections.swap(lists, i, i - 1);
+                }
+            }
+            adapter.notifyItemMoved(fromPosition, toPosition);
+            return true;
+        }
+
+        @Override
+        public void onSwiped(RecyclerView.ViewHolder viewHolder, int direction) {
+
+        }
+
+        /**
+         * 长按选中Item的时候开始调用
+         *
+         * @param viewHolder
+         * @param actionState
+         */
+        @Override
+        public void onSelectedChanged(RecyclerView.ViewHolder viewHolder, int actionState) {
+            if (actionState != ItemTouchHelper.ACTION_STATE_IDLE) {
+                viewHolder.itemView.setBackgroundColor(Color.LTGRAY);
+            }
+            super.onSelectedChanged(viewHolder, actionState);
+        }
+
+        /**
+         * 手指松开的时候还原
+         * @param recyclerView
+         * @param viewHolder
+         */
+        @Override
+        public void clearView(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder) {
+            super.clearView(recyclerView, viewHolder);
+            viewHolder.itemView.setBackgroundColor(0);
+        }
 
     }
+
 }
