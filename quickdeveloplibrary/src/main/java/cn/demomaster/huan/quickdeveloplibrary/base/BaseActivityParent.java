647ad9d14c03403d69afac8060839c3d989f3d04
squirrelhuan <978252909@qq.com>
2018-11-12 11:48:10 +0800

    actionbar处理

--- quickdeveloplibrary/src/main/java/cn/demomaster/huan/quickdeveloplibrary/base/BaseActivityParent.java
+++ quickdeveloplibrary/src/main/java/cn/demomaster/huan/quickdeveloplibrary/base/BaseActivityParent.java
@@ -1,16 +1,9 @@
 package cn.demomaster.huan.quickdeveloplibrary.base;
 
-import android.app.Activity;
-import android.content.Intent;
-import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
-import cn.demomaster.huan.quickdeveloplibrary.ApplicationParent;
 import cn.demomaster.huan.quickdeveloplibrary.base.tool.actionbar.ActionBarHelper;
 import cn.demomaster.huan.quickdeveloplibrary.base.tool.actionbar.ActionBarLayout;
-import cn.demomaster.huan.quickdeveloplibrary.util.StatusBarUtil;
 
 public class BaseActivityParent extends BaseActivityRoot {
 
--- quickdeveloplibrary/src/main/java/cn/demomaster/huan/quickdeveloplibrary/base/BaseActivityRoot.java
+++ quickdeveloplibrary/src/main/java/cn/demomaster/huan/quickdeveloplibrary/base/BaseActivityRoot.java
@@ -8,8 +8,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
 import cn.demomaster.huan.quickdeveloplibrary.ApplicationParent;
-import cn.demomaster.huan.quickdeveloplibrary.base.tool.actionbar.ActionBarHelper;
-import cn.demomaster.huan.quickdeveloplibrary.base.tool.actionbar.ActionBarLayout;
 import cn.demomaster.huan.quickdeveloplibrary.util.StatusBarUtil;
 
 public class BaseActivityRoot extends AppCompatActivity {
