439c93ccca76f46ca3e1c3dc5d23b888ec5395a5
squirrelhuan <978252909@qq.com>
2019-02-14 15:40:27 +0800

    update audio play

--- app/src/main/java/cn/demomaster/huan/quickdevelop/App.java
+++ app/src/main/java/cn/demomaster/huan/quickdevelop/App.java
@@ -1,22 +1,8 @@
 package cn.demomaster.huan.quickdevelop;
 
-import android.app.Activity;
-import android.support.annotation.NonNull;
-import android.util.Log;
-
-import com.alibaba.fastjson.JSON;
-
-import java.util.List;
-
-import cn.demomaster.huan.quickdevelop.net.RetrofitInterface;
 import cn.demomaster.huan.quickdeveloplibrary.ApplicationParent;
 import cn.demomaster.huan.quickdeveloplibrary.helper.NotifycationHelper;
 import cn.demomaster.huan.quickdeveloplibrary.helper.SoundHelper;
-import cn.demomaster.huan.quickdeveloplibrary.helper.toast.PopToastUtil;
-import cn.demomaster.huan.quickdeveloplibrary.http.HttpUtils;
-import io.reactivex.android.schedulers.AndroidSchedulers;
-import io.reactivex.observers.DisposableObserver;
-import io.reactivex.schedulers.Schedulers;
 
 public class App extends ApplicationParent {
 
@@ -26,8 +12,7 @@ public class App extends ApplicationParent {
 
         NotifycationHelper.getInstance().init(this);
         //SoundHelper.init(this);
-
-        SoundHelper.init(this,true,R.raw.class);//自动加载raw下的音频文件
+        SoundHelper.init(this, true, R.raw.class);//自动加载raw下的音频文件
 
     }
 }
