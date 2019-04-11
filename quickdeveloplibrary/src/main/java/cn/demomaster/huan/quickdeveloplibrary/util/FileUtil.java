912081c781627884d08d40b2cc0b98fca4f9fac3
squirrelhuan <978252909@qq.com>
2019-04-11 22:40:27 +0800

    moo photohelper 中选择图片并截图，有些手机效果是圆形区域截图的（夏普android8.0）

--- quickdeveloplibrary/src/main/java/cn/demomaster/huan/quickdeveloplibrary/util/FileUtil.java
+++ quickdeveloplibrary/src/main/java/cn/demomaster/huan/quickdeveloplibrary/util/FileUtil.java
@@ -190,7 +190,6 @@ public class FileUtil {
         String[] proj = {MediaStore.Images.Media.DATA};
         Cursor cursor = context.getContentResolver().query(contentUri, proj, null, null, null);
         if (cursor.moveToFirst()) {
-            ;
             int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
             res = cursor.getString(column_index);
         }
